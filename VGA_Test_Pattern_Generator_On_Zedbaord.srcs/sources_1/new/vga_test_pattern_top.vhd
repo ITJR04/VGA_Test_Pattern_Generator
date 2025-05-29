04fd59cd8d6cccf67d8b25a6b77458d6e7ea9897
Isai Torres <Itorres0591@gmail.com>
2025-05-29 14:03:20 -0400

    Update vga_test_pattern_top.vhd
    added comments and filled in description for details at top of module

--- VGA_Test_Pattern_Generator_On_Zedbaord.srcs/sources_1/new/vga_test_pattern_top.vhd
+++ VGA_Test_Pattern_Generator_On_Zedbaord.srcs/sources_1/new/vga_test_pattern_top.vhd
@@ -1,30 +1,43 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: Universtiy of Connecticut
+-- Engineer: Isai Torres
 -- 
 -- Create Date: 05/28/2025 12:25:39 PM
--- Design Name: 
+-- Design Name: VGA Test Pattern Top-Level 
 -- Module Name: vga_test_pattern_top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
+-- Project Name: VGA_Test_Pattern_Zedboard
+-- Target Devices: Zedboard Zynq-7000 (xc7z020clg484-1)
+-- Tool Versions: Vivado 2024.2
 -- Description: 
+--     This is the top-level wrapper module for the VGA test pattern project.
+--     It connects the 100 MHz onboard oscillator to a Clocking Wizard IP core
+--     to generate the 25 MHz VGA pixel clock, instantiates the test pattern
+--     generator, and drives output signals to VGA pins and onboard led.
+-- 
+--     led is used to display the status of the clock lock signal from the 
+--     MMCM to aid debugging. VGA sync and RGB signals are output through 
+--     dedicated ports connected to the VGA DAC. ( it helped me to figure out what my problem was)
 -- 
 -- Dependencies: 
--- 
+--     clk_wiz_0 (IP block for 25 MHz clock generation)
+--     vga_test_pattern.vhd
 -- Revision:
 -- Revision 0.01 - File Created
+-- Revision 0.02 - Integrated Clocking Wizard and video core for VGA output
 -- Additional Comments:
--- 
+--     Be sure resetn is tied high or appropriately handled in hardware
+--     Requires monitor to support 640x480 @ 60 Hz over VGA
+--     Designed to be simulated and synthesized independently
+--     in my VGA output i used a AOC C24G1 monitor to display the pattern
+
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -66,8 +79,8 @@ begin
     -- Instantiation of CLocking Wizard
     clkgen : clk_wiz_0
         port map (
-            clk_in1 => clk100MHz,
-            clk_out1 => clk25MHz,
+            clk_in1 => clk100MHz, -- input clock of 100MHz
+            clk_out1 => clk25MHz, -- output clock to be divided via PLL to 25MHz
             resetn => '1',
             locked => locked
        );
@@ -76,8 +89,8 @@ begin
     -- VGA timing and pattern generator
     vga_inst : entity work.vga_test_pattern 
         port map (
-            clk25MHz => clk25MHz,
-            reset => reset,
+            clk25MHz => clk25MHz, -- uses the 25MHz clock
+            reset => reset, 
             vga_hsync => vga_hsync,
             vga_vsync => vga_vsync,
             vga_red => pixel_r,
@@ -86,7 +99,7 @@ begin
         );
         
     -- connected output signals
-    vga_red <= pixel_r;
+    vga_red <= pixel_r; 
     vga_green <= pixel_g;
     vga_blue <= pixel_b;
     led <= locked; -- to see if the generated clock is stable
